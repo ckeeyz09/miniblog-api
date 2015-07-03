4e0bc73c586d513c8ee1a94967c57be6ed16642b
Sean Hill <TonkaTruck@Seans-MacBook-Air.local>
2015-07-03 11:48:18 -0700

    added more constructors

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-var User = function (name, age, location, image) {
+//CONSTRUCTORS
+
+var User = function (name, age, location, image,favorite_game,) {
   this.name = name;
   this.age = age;
   this.location = location;
-  this. = image;
+  this.image = image;
+  this.favGame = favorite_game;
   this.post = localStorage.getItem("post");
   this.key = "post";
 }
 
+var Company = function (name, location, size, titles, image) {
+  this.name = name;
+  this.location = location;
+  this.size = size;
+  this.titles = titles;
+  this.image = image;
+}
 
+var Developer = function (name, job, company, current_project) {
+  this.name = name;
+  this.job = job;
+  this.company = company;
+  this.project = current_project;
+}
+
+//LOCAL STORAGE
 
 function SaveRender() {}
 
@@ -25,6 +43,8 @@ SaveRender.prototype.saveToLs = function (input) {
 
 }
 
+//RENDER TO TEMPLATE
+
 SaveRender.prototype.renderTemplate = function (template_source, where) {
 
   var items_json = JSON.parse(this.items);
@@ -35,3 +55,22 @@ SaveRender.prototype.renderTemplate = function (template_source, where) {
   });
 
 }
+//INPUT FIELD
+
+// input field animation & expansion
+$('#post-input').focus(function()
+{
+    /*to make this flexible, I'm storing the current width in an attribute*/
+    $(this).attr('data-default', $(this).width());
+    $(this).animate({ width: 700 }, 'slow');
+    $(this).attr('data-default', $(this).height());
+    $(this).animate({ height: 150 }, 'fast');
+});
+$('#post-input').blur(function()
+{
+    /* lookup the original width */
+    var w = $(this).attr('data-default');
+    var h = $(this).attr('data-default');
+    $(this).animate({ height: h }, 'fast');
+    $(this).animate({ width: w }, 'slow');
+});

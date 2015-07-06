e966584d8d64612db7ce5ea0ddfe7d2a1ea2b61e
Sean Hill <TonkaTruck@Seans-MacBook-Air.local>
2015-07-05 19:54:05 -0700

    version to submit. Not working

--- script2.js
+++ script2.js
@@ -7,7 +7,7 @@ var $newStatus = $("#status-modal-form");
 var statusTemplate = _.template($("#status-template").html());
 
 var Status = function (image, game, status) {
-  this.postImage = image;
+  this.image = image;
   this.game = game;
   this.status = status;
   this.items = localStorage.getItem("status");
@@ -18,45 +18,39 @@ Status.all = [];
 
 Status.prototype.saveToLs = function (input) {
 
-  // if (this.items) {
-  //   items_json = JSON.parse(this.items)
-  // }
-  // else {
-  //   items_json = [];
-  // }
-  // items_json.push(input);
+  if (this.items) {
+    items_json = JSON.parse(this.items)
+  }
+  else {
+    items_json = [];
+  }
+  items_json.push(input);
 
-  // localStorage.setItem(this.key, JSON.stringify(items_json));
-  Status.all.push(this);
+  localStorage.setItem(this.key, JSON.stringify(items_json));
 }
 
 Status.prototype.render = function(template_source, where) {
 
-  // var items_json = JSON.parse(this.items);
-  // var template = _.template($(template_source).html());
+  var items_json = JSON.parse(this.items);
+  var template = _.template($(template_source).html());
 
-  // _.each(items_json, function(item) {
-  //   $(where).append(template(item));
-  // });
-    var postIndex = Status.all.indexOf(this);
-    var $status = $(statusTemplate(this));
-    $post.attr("data-index", postIndex);
-    $postList.append($post);
+  _.each(items_json, function(item) {
+    $(where).append(template(item));
+  });
 
 }
 
 
 $newStatus.on("submit", function() {
-
+  event.preventDefault();
   var statusImage = $("#new-image").val();
   var statusGame = $("#new-game").val();
   var statusBody = $("#new-body").val();
   var newStatus = new Status(statusImage, statusGame, statusBody);
 
-  // newStatus.saveToLs(newStatus);
-  // newStatus.render('#status-template', '#status-container');
-  newStatus.saveToLs();
-  newStatus.render();
+  newStatus.saveToLs(newStatus);
+  newStatus.render('#status-template', '#status-container');
+
 });
 
 

eee6155bcaf4ab17be2b8845eeb39d10a9b1bace
Sean Hill <TonkaTruck@Seans-MacBook-Air.local>
2015-07-05 19:42:51 -0700

    minor tweaks. Still not working

--- script2.js
+++ script2.js
@@ -1,4 +1,8 @@
+$(function() {
 
+
+
+var $postList = $("#post-list");
 var $newStatus = $("#status-modal-form");
 var statusTemplate = _.template($("#status-template").html());
 
@@ -14,37 +18,46 @@ Status.all = [];
 
 Status.prototype.saveToLs = function (input) {
 
-  if (this.items) {
-    items_json = JSON.parse(this.items)
-  }
-  else {
-    items_json = [];
-  }
-  items_json.push(input);
+  // if (this.items) {
+  //   items_json = JSON.parse(this.items)
+  // }
+  // else {
+  //   items_json = [];
+  // }
+  // items_json.push(input);
 
-  localStorage.setItem(this.key, JSON.stringify(items_json));
-  // Status.all.push(this);
+  // localStorage.setItem(this.key, JSON.stringify(items_json));
+  Status.all.push(this);
 }
 
 Status.prototype.render = function(template_source, where) {
 
-  var items_json = JSON.parse(this.items);
-  var template = _.template($(template_source).html());
+  // var items_json = JSON.parse(this.items);
+  // var template = _.template($(template_source).html());
 
-  _.each(items_json, function(item) {
-    $(where).append(template(item));
-  });
+  // _.each(items_json, function(item) {
+  //   $(where).append(template(item));
+  // });
+    var postIndex = Status.all.indexOf(this);
+    var $status = $(statusTemplate(this));
+    $post.attr("data-index", postIndex);
+    $postList.append($post);
+
 }
 
 
 $newStatus.on("submit", function() {
-  event.preventDefault();
 
   var statusImage = $("#new-image").val();
   var statusGame = $("#new-game").val();
   var statusBody = $("#new-body").val();
   var newStatus = new Status(statusImage, statusGame, statusBody);
 
-  newStatus.saveToLs(newStatus);
-  newStatus.render('#status-template', '#status-container');
+  // newStatus.saveToLs(newStatus);
+  // newStatus.render('#status-template', '#status-container');
+  newStatus.saveToLs();
+  newStatus.render();
+});
+
+
 });
